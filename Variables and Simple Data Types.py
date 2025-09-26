75d9724e417679e8417f7309fd43535e3423cd6b
relaiza <gamuyodrelaiza@gmail.com>
2025-09-26 21:41:15 +0100

    Add files via upload
    exercises form Python Crash Course

---
+++ Variables and Simple Data Types.py
@@ -0,0 +1,9 @@
+#Simple Message: Assign a message to a variable, and then print that message.
+cutie = "I love cute things"
+print(cutie)
+
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+manila = "manila is a very hot place"
+print(manila)
+manila = "manila can also be cold during the night"
+print(manila)
