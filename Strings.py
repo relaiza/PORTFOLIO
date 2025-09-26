97e237e33e4ed3f36069a739d49bf6d50d679844
relaiza <gamuyodrelaiza@gmail.com>
2025-09-26 21:55:12 +0100

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,9 @@
+relaiza = "Hello Relaiza, would you like to learn some Python today?"
+print (relaiza)
+
+name= "relaiza gamuyod"
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+print('In the bible is is written that “Come to me, all you who are weary and burdened, and I will give you rest. 29 Take my yoke upon you and learn from me, for I am gentle and humble in heart, and you will find rest for your souls. 30 For my yoke is easy and my burden is light.”')
