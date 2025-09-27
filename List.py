c764fc899391e1ae6b75fa080657e8b774e08756
relaiza <gamuyodrelaiza@gmail.com>
2025-09-27 18:27:32 +0100

    Add files via upload
    from Python Crash Course

---
+++ List.py
@@ -0,0 +1,15 @@
+#ex 3.1
+names = ['Benize', 'Chelsie', 'Keya', 'KC', 'Kriel']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+#ex 3.2
+print("Hello ate " + names[0] + " you are such a reliable friend.")
+print("Hello ate " + names[1] + " you are so fun to be with!")
+print("Hello " + names[2] + "! I hope you well for your journey in Portsmouth university!")
+print("Hellooooo baby " + names[3] + "!!! I love you so much!!")
+print("Hello " + names[4] + "! I hope you all well for nursing!")
+
