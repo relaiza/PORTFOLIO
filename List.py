366d00bf3a5b0fe2cd7a63bb7c7b419a3a73c611
relaiza <gamuyodrelaiza@gmail.com>
2025-09-28 21:12:43 +0100

    Update List.py

--- List.py
+++ List.py
@@ -4,12 +4,45 @@ print(names[0])
 print(names[1])
 print(names[2])
 print(names[3])
-print(names[4])
+print(names[4] + ".\n")
 
 #ex 3.2
 print("Hello ate " + names[0] + " you are such a reliable friend.")
 print("Hello ate " + names[1] + " you are so fun to be with!")
 print("Hello " + names[2] + "! I hope you well for your journey in Portsmouth university!")
 print("Hellooooo baby " + names[3] + "!!! I love you so much!!")
-print("Hello " + names[4] + "! I hope you all well for nursing!")
+print("Hello " + names[4] + "! I hope you all well for nursing!" + '.\n')
+
+pizzas = ['pepporini', 'hawaian', 'cheese']
+
+for pizza in pizzas:
+    print(f' I like {pizza.title()} is and I enjoy eating it!')
+print('I really like pizzas!'+ ".\n")
+
+animals = ['dog', 'cat', 'fish']
+for animal in animals:
+    print(f'I think {animal} will be a great pet!')
+print("any of this animals would make a great pet" + ".\n")
+
+print("The first three items in the list are:")
+print(names[:3])
+print("\nThe items from the middle of the list are:")
+print(names[1:4])
+print("\nLast three items from the list are:")
+print(names[-3:])
+
+pizzas.append("deluxe")
+friend_pizzas = pizzas.copy()
+friend_pizzas.append('mighty meaty')
+
+for pizza in pizzas:
+    print("\nMy fabourite pizzas are:")
+    print(pizzas)
+    print("\nMy friend's favourite pizzas are:")
+    print(friend_pizzas)
+
+
+
+
+
 
